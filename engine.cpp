c7e50d8c87742d2d4c196a133ef0e7baadbce3c3
Ishan Agarwal <ishanagarwal66@yahoo.in>
2021-09-09 16:06:44 +0530

    Initial Commit

--- engine.cpp
+++ engine.cpp
@@ -1,5 +1,9 @@
 #include<bits/stdc++.h>
+#include "board.cpp"
+
+string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w Qkq e4 32 132";
 using namespace std;
 int main(){
-    cout<<"Chess-Engine";
+    board game;
+    game.ParseFEN(fen);
 }
